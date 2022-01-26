de19b924eb023997cb3c886c44f08b1f5a6b33f2
Pochesh devu <pocheshd@gmail.com>
2022-01-26 16:49:26 +0530

    first commit

---
+++ src/conversion/conversion.java
@@ -0,0 +1,15 @@
+package conversion;
+
+public class conversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 100;
+		long l = i;
+		float f = l;
+		System.out.println("Int value "+l);
+		System.out.println("long value "+l);
+		System.out.println("float value "+f);
+	}
+
+}
